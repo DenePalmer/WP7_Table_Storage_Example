fa371eef0dd47cfd6cbb967d4225a504df8d227c
Dene Palmer <denep123@googlemail.com>
2013-07-27 11:07:32 +0100

    Added code files

---
+++ Assignment2App/Assignment2App/Pages/addCD.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Assignment2App.ServiceReference1;
+
+namespace Assignment2App.Pages
+{
+    public partial class addCD : PhoneApplicationPage
+    {
+        public addCD()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // set value for uploading to cloud SQL database for YOUR UNIQUE guid
+            string storageGuid = "Guid: c9c5673b-e773-441d-acaa-968095b28662";
+            Guid storageId = Guid.Parse(storageGuid);
+            // create new serviceclient instance for our cloud storage service and upload our data using AddImageAsync
+            Service1Client svc = new Service1Client();
+            svc.AddCDCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(svc_AddCDCompleted);
+            // pass our guid, and title, artist and year from textblocks
+            string artist = ("Artist: " + textBox1.Text);
+            string title = ("Title: " + textBox2.Text);
+            string year = ("Year: " + textBox3.Text);
+            //svc.AddCDAsync(storageId, textBox1.Text, textBox2.Text, textBox3.Text);
+            svc.AddCDAsync(storageId, artist, title, year);
+         }
+
+        // displays message if new profile has been added  to database successfully
+        void svc_AddCDCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                MessageBox.Show("CD info added!", "Success", MessageBoxButton.OK);
+            }
+            else
+            {
+                MessageBox.Show("Problem adding CD info", "Unsuccessful", MessageBoxButton.OK);
+                Console.WriteLine("An error occured:" + e.Error);
+            }
+        }
+        
+    }
+}
---
+++ Assignment2App/Assignment2App/Pages/viewCollection.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Assignment2App.ServiceReference1;
+
+namespace Assignment2App.Pages
+{
+    public partial class viewCollection : PhoneApplicationPage
+    {
+        public viewCollection()
+        {
+            InitializeComponent();
+            // make call to SQL service when page loads
+            // request to view all cd info and bind to listbox control
+
+            Service1Client svc = new Service1Client();
+            svc.ViewInfoCompleted += new EventHandler<ViewInfoCompletedEventArgs>(svc_ViewInfoCompleted);
+            svc.ViewInfoAsync();
+        }
+
+        void svc_ViewInfoCompleted(object sender, ViewInfoCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+
+                // bind profiles to listbox
+                listBox1.ItemsSource = e.Result;
+                MessageBox.Show("CD collection downloaded!", "Success", MessageBoxButton.OK);
+            }
+            else
+            {
+                MessageBox.Show("Problem viewing collection", "Unsuccessful", MessageBoxButton.OK);
+                Console.WriteLine("An error occured:" + e.Error);
+            }
+        }
+    }
+}
